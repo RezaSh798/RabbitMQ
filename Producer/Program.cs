b3354a88ae85dd767b784a0b7ce9406859b7b1eb
RezaSh798 <rezakhorramisalarabadi@gmail.com>
2025-06-26 16:16:30 +0330

    Feature: Write producer and consumer project.

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    // UserName = "test",
+    // Password = "test"
+};
+
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(
+    queue: "news",
+    durable: true, // Should this queue survive a broker restart?
+    exclusive: false, // Should this queue use be limited to its declaring connection? Such a queue will be deleted when its declaring connection closes.
+    autoDelete: false, // Should this queue be auto-deleted when its last consumer (if any) unsubscribes?
+    arguments: null // Optional; additional queue arguments, e.g. "x-queue-type"
+);
+
+while (true)
+{
+    var message = $"{DateTime.UtcNow} - {Guid.CreateVersion7()}";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    await channel.BasicPublishAsync(
+        exchange: string.Empty,
+        routingKey: "news",
+        mandatory: true,
+        basicProperties: new BasicProperties { Persistent = true },
+        body
+    );
+
+    Console.WriteLine($"Sent: {message}");
+
+    await Task.Delay(2000);
+}
--- Subscriber/Program.cs
+++ Subscriber/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    // UserName = "test",
+    // Password = "test"
+};
+
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(
+    queue: "news",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+Console.WriteLine("Waiting for messages");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (sender, eventArgs) =>
+{
+    byte[] body = eventArgs.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"Received: {message}");
+
+    await ((AsyncEventingBasicConsumer)sender)
+        .Channel.BasicAckAsync(eventArgs.DeliveryTag, multiple: false);
+};
+
+await channel.BasicConsumeAsync("news", false, consumer);
